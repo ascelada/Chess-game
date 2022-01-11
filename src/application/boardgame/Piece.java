377c3ce7938050f76ff36cfb4b3cfdd61c35ebee
Andreas Celada <andreas.sch.celada@gmail.com>
2022-01-11 19:07:13 -0300

    Piece and Board Creation

---
+++ src/application/boardgame/Piece.java
@@ -0,0 +1,19 @@
+package application.boardgame;
+
+public abstract class Piece {
+
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+        position=null;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+
+
+
+}
